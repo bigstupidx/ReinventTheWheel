c44e8e99854ff249a33d18146669b169aadfe8b7
Ken Miller <ken.miller521@gmail.com>
2017-11-03 20:33:03 -0700

    Made main menu and options menu

---
+++ Assets/Scripts/MainMenuNavigationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuNavigationController : MonoBehaviour {
+
+	public void EnablePanel(GameObject panel)
+    {
+        panel.SetActive(true);
+    }
+    public void DisablePanel(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
