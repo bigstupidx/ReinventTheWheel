0a999518822d367f5faaac4ab79db91066f0a860
Ken Miller <ken.miller521@gmail.com>
2017-11-07 22:03:43 -0800

    Worked on Main Menu UI with sounds

--- Assets/Scripts/MainMenuNavigationController.cs
+++ Assets/Scripts/MainMenuNavigationController.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuNavigationController : MonoBehaviour {
+    public Image blackoutPanel;
+    public float fadeSpeed;
+    private AudioSource UIAudioSource;
 
     void Start()
     {
+        
+        UIAudioSource = GameObject.Find("UIAudioObject").GetComponent<AudioSource>();
         if (Time.timeScale == 0)
             Time.timeScale = 1;
     }
@@ -24,6 +30,17 @@ public class MainMenuNavigationController : MonoBehaviour {
     }
     public void LoadScene(string sceneName)
     {
+        StartCoroutine(ChangeScene(sceneName));
+    }
+    IEnumerator ChangeScene(string sceneName)
+    {
+        blackoutPanel.gameObject.SetActive(true);
+        blackoutPanel.canvasRenderer.SetAlpha(0.0f);
+        blackoutPanel.CrossFadeAlpha(1.0f, fadeSpeed, false);
+        while(blackoutPanel.canvasRenderer.GetAlpha() < 1.0f)
+        {
+            yield return null;
+        }
         SceneManager.LoadScene(sceneName);
     }
 }
