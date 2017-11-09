2d812c33bb7bb29962348378817b91a633ed2c52
Stanley Ung <StanleyUng167@gmail.com>
2017-11-08 19:00:53 -0800

    Working on the point system
    For now the point system is calculated by checking the current position
    and then checking the next position at a certain frame and subtracting.

---
+++ Assets/Scripts/PointTracker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointTracker : MonoBehaviour {
+
+    public GameObject player;
+    private Transform boulderPosition;
+    private bool inMotion;
+
+    // Testing
+    public float currentPosition;
+    public float finalPosition;
+
+    public float points = 0;
+
+	// Use this for initialization
+	void Start () {
+        boulderPosition = player.GetComponent<Transform>();
+        currentPosition = boulderPosition.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        //currentPosition = boulderPosition.transform.position.x - currentPosition;
+        if (!player.GetComponent<Rigidbody2D>().IsSleeping()) {
+            Debug.Log(boulderPosition.transform.position.x + " - " +currentPosition);
+            points += boulderPosition.transform.position.x - currentPosition;
+        }
+        //points += boulderPosition.transform.position.x - currentPosition;
+
+        if (player.GetComponent<Rigidbody2D>().IsSleeping()) {
+            
+            Debug.Log(currentPosition);
+        }
+	}
+}
