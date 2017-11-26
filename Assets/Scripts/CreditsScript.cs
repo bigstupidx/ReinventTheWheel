52175f9f57cd0298d59918bdffccce71ad7be11e
Ken Miller <ken.miller521@gmail.com>
2017-11-26 13:03:40 -0800

    Made credits

--- Assets/Scripts/CreditsScript.cs
+++ Assets/Scripts/CreditsScript.cs
@@ -2,11 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CreditsScript : MonoBehaviour {
-    
-    public void LoadScene(string scene)
+    public Text text;
+    public float textFadeInSpeed = 0.5f;
+    public float textVisibleTime = 1.0f;
+    public Animator cameraAnimator;
+    private Coroutine creditsCoroutine;
+    void Start()
     {
-        SceneManager.LoadScene(scene);
+        text.canvasRenderer.SetAlpha(0.0f);
+        //StartCoroutine(CreditsRolling());
+    }
+    void Update()
+    {
+
+    }
+    public void startCredits()
+    {
+        creditsCoroutine = StartCoroutine(CreditsRolling());
+    }
+    public void stopCredits()
+    {
+        StopCoroutine(creditsCoroutine);
+        text.CrossFadeAlpha(0.0f, textFadeInSpeed, true);
+    }
+    IEnumerator CreditsRolling()
+    {
+        text.text = "Thank you for playing\n\nAn original idea by\nJonathan West";
+        text.CrossFadeAlpha(1.0f, textFadeInSpeed, true);
+        yield return new WaitForSeconds(textVisibleTime);
+        text.CrossFadeAlpha(0.0f, textFadeInSpeed, true);
+        yield return new WaitForSeconds(textFadeInSpeed);
+        text.text = "Producer\nKen Miller\n\nArt Director\nKatrina Yi";
+        text.CrossFadeAlpha(1.0f, textFadeInSpeed, true);
+        yield return new WaitForSeconds(textVisibleTime);
+        text.CrossFadeAlpha(0.0f, textFadeInSpeed, true);
+        yield return new WaitForSeconds(textFadeInSpeed);
+        text.text = "Programming\nKen Miller\nRuben Sanchez\nJuan Alvarez\nStanley Ung\nWolfgang Hellickson";
+        text.CrossFadeAlpha(1.0f, textFadeInSpeed, true);
+        yield return new WaitForSeconds(textVisibleTime);
+        text.CrossFadeAlpha(0.0f, textFadeInSpeed, true);
+        yield return new WaitForSeconds(textFadeInSpeed);
+        text.text = "Art\nKatrina Yi\nNathan Xa\nSarah Cho\nYuuchi Ng\n\nSound Design and Composition\nDaniel Ramos\n\nGame Design\nJonathan West";
+        text.CrossFadeAlpha(1.0f, textFadeInSpeed, true);
+        yield return new WaitForSeconds(textVisibleTime);
+        text.CrossFadeAlpha(0.0f, textFadeInSpeed, true);
+        yield return new WaitForSeconds(textFadeInSpeed);
+        cameraAnimator.SetBool("isOnCredits", false);
+        
     }
 }
