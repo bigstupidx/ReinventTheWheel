b46064fab32ae2d86f972c73bb8b71a4eeb578f5
j95alvarez <juanalvarez391@yahoo.com>
2017-12-01 01:22:40 -0800

    added mammoth animation
    implemented mammoth animation

--- Assets/MammothController.cs
+++ Assets/MammothController.cs
@@ -9,13 +9,16 @@ public class MammothController : MonoBehaviour
     public  void ReleaseTheMammoth()
     {
         GameObject mammoth = GameObject.FindGameObjectWithTag("Mammoth");
-        mammoth.GetComponent<Rigidbody2D>().velocity = Vector2.right * mammothSpeed; 
+        mammoth.GetComponent<Rigidbody2D>().velocity = Vector2.right * mammothSpeed;
+        mammoth.GetComponent<Animator>().SetBool("isMoving", true);
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Boulder")
         {
+            GameObject mammoth = GameObject.FindGameObjectWithTag("Mammoth");
+            mammoth.GetComponent<Animator>().SetBool("HitsRock", true);
             other.GetComponent<Rigidbody2D>().gravityScale = 1;
         }
     }
